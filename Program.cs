21d73a8dcfa5636d5888238c55a44d71bebbfcb8
Chris Terrazas <chris.terrazas@gmail.com>
2019-03-04 14:06:37 -0500

    Added some variables, while, for loop

--- Program.cs
+++ Program.cs
@@ -6,8 +6,34 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            // int score = 5;
+            // // float fl = 5.5f;
+            // // decimal dec;
+            // // double dou;
 
-            Console.WriteLine("Hello World, welcomd to .net!");
+            // score = 5.5;
+
+            var score = 5;
+            score = 12;
+            score++;
+            score += 6;
+
+            Console.WriteLine(score);
+
+            // loops
+            // if 
+            var isRunning = true;
+
+            while(score<22){
+                score++;
+            }
+            Console.WriteLine(score);
+
+            for (var i = 0; i < 10; i++){
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Hello World, welcome to .net!");
         }
     }
 }
